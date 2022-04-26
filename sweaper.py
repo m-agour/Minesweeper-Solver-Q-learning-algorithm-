ea15dbf644441819baf2f0c8c6223bafa5578404
m-agour <mo.aggour@gmail.com>
2022-04-26 11:45:04 +0200

    Update sweaper.py

--- sweaper.py
+++ sweaper.py
@@ -13,7 +13,23 @@ states = {
 }
 # will i need flag?
 
+
 # lets make zeros more than ones
-mine_grid = [[choice(3 * [0] + [1]) for i in range(9)] for j in range(9)]
-player_grid = [[-1 for i in range(9)] for j in range(9)]
-print(mine_grid)
+
+class Sweeper:
+    def __init__(self, w, h):
+        self.w = w
+        self.h = h
+        self.mines_grid = [[choice(3 * [0] + [1]) for i in range(self.h)] for j in range(self.w)]
+        self.player_grid = [[-1 for x in range(self.h)] for y in range(self.w)]
+
+    def count_neighbours(self, row, col):
+        c = 0
+        for i in range(row-1, row+2):
+            for j in range(col - 1, col + 2):
+                if (0 <= i <= 8) and (0 <= j <= 8):
+                    if self.mines_grid[i][j] == MINE:
+                        c += 1
+        return c
+
+    
