8c1476b57d00ba0f2f3d3a0fa376cdfff84bc1cb
m-agour <mo.aggour@gmail.com>
2022-04-26 12:04:04 +0200

    Update sweaper.py

--- sweaper.py
+++ sweaper.py
@@ -23,13 +23,36 @@ class Sweeper:
         self.mines_grid = [[choice(3 * [0] + [1]) for i in range(self.h)] for j in range(self.w)]
         self.player_grid = [[-1 for x in range(self.h)] for y in range(self.w)]
 
-    def count_neighbours(self, row, col):
+    def count_neighbours(self, x, y):
         c = 0
-        for i in range(row-1, row+2):
-            for j in range(col - 1, col + 2):
+        for i in range(x-1, y+2):
+            for j in range(x - 1, y + 2):
                 if (0 <= i <= 8) and (0 <= j <= 8):
                     if self.mines_grid[i][j] == MINE:
                         c += 1
         return c
 
-    
+    def reveal(self, x, y):
+        if self.mines_grid[x][y] == MINE:
+            print("BOOM!")
+        elif self.player_grid[x][y] == UNKNOWN:
+            self.player_grid[x][y] = self.count_neighbours(x, y)
+
+            # Can this be recursive?
+            cells = [(x, y)]
+            while len(cells) > 0:
+                cell = cells.pop()
+                x = cell[0]
+                y = cell[1]
+                for i in range(x - 1, x + 2):
+                    for j in range(y - 1, y + 2):
+                        if (0 <= i <= 8) and (0 <= j <= 8):
+                            if (self.player_grid[i][j] == UNKNOWN) and (self.mines_grid[i][j] == EMPTY):
+                                self.player_grid[i][j] = self.count_neighbours(i, j)
+                                if self.count_neighbours(i, j) == EMPTY and (i, j) not in cells:
+                                    cells.append((i, j))
+                                else:
+                                    self.player_grid[i][j] = self.count_neighbours(i, j)
+
+
+
